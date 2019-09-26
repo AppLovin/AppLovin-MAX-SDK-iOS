0469dbd9dafe1b1740116e02b9cabc6b534d0e96
Andrew Tian <tiandrew@umich.edu>
2019-09-26 16:51:18 -0700

    Update ALAppDelegate.swift

--- DemoApp-Swift/DemoApp-Swift/ALAppDelegate.swift
+++ DemoApp-Swift/DemoApp-Swift/ALAppDelegate.swift
@@ -9,7 +9,6 @@
 import UIKit
 import AppLovinSDK
 
-@UIApplicationMain
 class ALAppDelegate: UIResponder, UIApplicationDelegate
 {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
