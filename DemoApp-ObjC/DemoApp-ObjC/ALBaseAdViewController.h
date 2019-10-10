c203d6b12cbac34d310405ae4e49340005b1b217
Harry Arakkal <harry.arakkal@applovin.com>
2019-10-10 12:10:09 -0700

    Add callback table to every ad view controller

---
+++ DemoApp-ObjC/DemoApp-ObjC/ALBaseAdViewController.h
@@ -0,0 +1,22 @@
+//
+//  ALBaseAdViewController.h
+//  DemoApp-ObjC
+//
+//  Created by Harry Arakkal on 10/9/19.
+//  Copyright © 2019 AppLovin Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ALBaseAdViewController : UIViewController
+
+/**
+ * Used for logging ad callbacks in the callback table.
+ */
+- (void)logCallback:(const char *)name;
+
+@end
+
+NS_ASSUME_NONNULL_END
