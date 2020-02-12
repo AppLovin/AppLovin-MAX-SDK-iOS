16ba0f488c96d2b3558f4d51d4e411e3cd6fc4f1
Varsha Hanji <varsha.hanji@applovin.com>
2020-02-12 11:22:56 -0800

    MRECs changes

---
+++ DemoApp-ObjC/DemoApp-ObjC/AppLovin/MRECs/ALDemoProgrammaticMRECViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALDemoProgrammaticMRECViewController.h
+//  iOS-SDK-Demo-ObjC
+//
+//  Created by Thomas So on 3/6/17.
+//  Copyright © 2017 AppLovin. All rights reserved.
+//
+
+#import "ALBaseAdViewController.h"
+
+@interface ALDemoProgrammaticMRECViewController : ALBaseAdViewController
+
+@end
