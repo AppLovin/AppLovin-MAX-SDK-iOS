3962497fb4283d75e027744b9834281584c65921
Varsha Hanji <varsha.hanji@applovin.com>
2020-04-06 22:02:12 -0700

    Adding interface builder for MRecs in Swift

---
+++ AppLovin MAX Demo App - Swift/AppLovin MAX Demo App - Swift/AppLovin/Mrecs/ALDemoInterfaceBuilderMRECViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ALDemoInterfaceBuilderMRECViewController.swift
+//  AppLovin MAX Demo App - Swift
+//
+//  Created by Varsha Hanji on 4/6/20.
+//  Copyright © 2020 AppLovin. All rights reserved.
+//
+
+import UIKit
+import AppLovinSDK
+
+class ALDemoInterfaceBuilderMRECViewController : ALBaseAdViewController
+{
+    
+    @IBOutlet weak var adView: ALAdView!
+    
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        adView.adLoadDelegate = self
+        adView.adDisplayDelegate = self
+        adView.adEventDelegate = self
+
+        adView.adSize = ALAdSize.mrec
+
+        // Call loadNextAd() to start showing ads
+        adView.loadNextAd()
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool)
+    {
+        super.viewDidDisappear(animated)
+        
+        adView.adLoadDelegate = nil
+        adView.adDisplayDelegate = nil
+        adView.adEventDelegate = nil
+    }
+}
+
+extension ALDemoInterfaceBuilderMRECViewController : ALAdLoadDelegate
+{
+    func adService(_ adService: ALAdService, didLoad ad: ALAd)
+    {
+        logCallback()
+    }
+    
+    func adService(_ adService: ALAdService, didFailToLoadAdWithError code: Int32)
+    {
+        // Look at ALErrorCodes.h for list of error codes
+        logCallback()
+    }
+}
+
+extension ALDemoInterfaceBuilderMRECViewController : ALAdDisplayDelegate
+{
+    func ad(_ ad: ALAd, wasDisplayedIn view: UIView)
+    {
+        logCallback()
+    }
+    
+    func ad(_ ad: ALAd, wasHiddenIn view: UIView)
+    {
+        logCallback()
+    }
+    
+    func ad(_ ad: ALAd, wasClickedIn view: UIView)
+    {
+        logCallback()
+    }
+}
+
+extension ALDemoInterfaceBuilderMRECViewController : ALAdViewEventDelegate
+{
+    func ad(_ ad: ALAd, didPresentFullscreenFor adView: ALAdView)
+    {
+        logCallback()
+    }
+    
+    func ad(_ ad: ALAd, willDismissFullscreenFor adView: ALAdView)
+    {
+        logCallback()
+    }
+    
+    func ad(_ ad: ALAd, didDismissFullscreenFor adView: ALAdView)
+    {
+        logCallback()
+    }
+    
+    func ad(_ ad: ALAd, willLeaveApplicationFor adView: ALAdView)
+    {
+        logCallback()
+    }
+    
+    func ad(_ ad: ALAd, didReturnToApplicationFor adView: ALAdView)
+    {
+        logCallback()
+    }
+    
+    func ad(_ ad: ALAd, didFailToDisplayIn adView: ALAdView, withError code: ALAdViewDisplayErrorCode)
+    {
+        logCallback()
+    }
+}
