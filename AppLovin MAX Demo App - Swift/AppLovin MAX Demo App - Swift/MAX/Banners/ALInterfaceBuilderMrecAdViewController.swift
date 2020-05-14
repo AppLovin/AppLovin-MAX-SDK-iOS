6c16ea5e725615d6ce310727909f405eedc5b3dc
Andrew Tian <tiandrew@umich.edu>
2020-05-14 03:20:04 -0700

    max mrec support

---
+++ AppLovin MAX Demo App - Swift/AppLovin MAX Demo App - Swift/MAX/Banners/ALInterfaceBuilderMrecAdViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ALInterfaceBuilderMrecAdViewController.swift
+//  DemoApp-Swift
+//
+//  Created by Andrew Tian on 1/23/20.
+//  Copyright © 2020 AppLovin. All rights reserved.
+//
+
+import UIKit
+import AppLovinSDK
+
+class ALInterfaceBuilderMrecAdViewController: ALBaseAdViewController, MAAdViewAdDelegate
+{
+    @IBOutlet weak var adView: MAAdView!
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        // NOTE: Must set Storyboard "User Defined Runtime Attributes" for MREC ad view
+        // Key Path = ad_unit_id & ad_format
+        // Type     = String
+        // Value    = YOUR_AD_UNIT_ID & MREC
+        
+        // Load the first ad
+        adView.loadAd()
+    }
+    
+    // MARK: MAAdDelegate Protocol
+    
+    func didLoad(_ ad: MAAd) { logCallback() }
+    
+    func didFailToLoadAd(forAdUnitIdentifier adUnitIdentifier: String, withErrorCode errorCode: Int) { logCallback() }
+
+    func didDisplay(_ ad: MAAd) { logCallback() }
+    
+    func didHide(_ ad: MAAd) { logCallback() }
+    
+    func didClick(_ ad: MAAd) { logCallback() }
+    
+    func didFail(toDisplay ad: MAAd, withErrorCode errorCode: Int) { logCallback() }
+    
+    // MARK: MAAdViewAdDelegate Protocol
+    
+    func didExpand(_ ad: MAAd) { logCallback() }
+    
+    func didCollapse(_ ad: MAAd) { logCallback() }
+}
