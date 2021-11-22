e08036e6c53a2d76fdd0338cba53e652a383e73e
NanaAmoah <90287918+NanaAmoah@users.noreply.github.com>
2021-11-22 10:28:43 -0800

    Mrec Additions - ObjC

---
+++ AppLovin MAX Demo App - ObjC/AppLovin MAX Demo App - ObjC/AppLovin/MRECs/Programmatic/ALDemoProgrammaticMrecViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALDemoProgrammaticMrecViewController.h
+//  AppLovin MAX Demo App - ObjC
+//
+//  Created by Nana Amoah on 11/18/21.
+//  Copyright © 2021 AppLovin Corporation. All rights reserved.
+//
+
+#import "ALBaseAdViewController.h"
+
+@interface ALDemoProgrammaticMrecViewController : ALBaseAdViewController
+
+@end
