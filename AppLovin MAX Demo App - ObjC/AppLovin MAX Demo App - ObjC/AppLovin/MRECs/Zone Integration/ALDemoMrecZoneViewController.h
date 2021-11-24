f142cb2805e5b9f95e7ddccacb84d18b57e72193
NanaAmoah <90287918+NanaAmoah@users.noreply.github.com>
2021-11-24 04:43:20 -0800

    Added Zone View Files - Obj-C

---
+++ AppLovin MAX Demo App - ObjC/AppLovin MAX Demo App - ObjC/AppLovin/MRECs/Zone Integration/ALDemoMrecZoneViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALDemoMrecZoneViewController.h
+//  AppLovin MAX Demo App - ObjC
+//
+//  Created by Nana Amoah on 11/23/21.
+//  Copyright © 2021 AppLovin Corporation. All rights reserved.
+//
+
+#import "ALBaseAdViewController.h"
+
+@interface ALDemoMrecZoneViewController : ALBaseAdViewController
+
+@end
