a169a108ce9bcb1582fffe557721e1d9597c58f0
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2022-04-01 15:35:45 -0700

    GoogleAdManager/9.2.0.0

--- GoogleAdManager/GoogleAdManagerAdapter/ALGoogleAdManagerMediationAdapter.m
+++ GoogleAdManager/GoogleAdManagerAdapter/ALGoogleAdManagerMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALGoogleAdManagerMediationAdapter.h"
 #import <GoogleMobileAds/GoogleMobileAds.h>
 
-#define ADAPTER_VERSION @"9.1.0.0"
+#define ADAPTER_VERSION @"9.2.0.0"
 
 @interface ALGoogleAdManagerInterstitialDelegate : NSObject<GADFullScreenContentDelegate>
 @property (nonatomic,   weak) ALGoogleAdManagerMediationAdapter *parentAdapter;
