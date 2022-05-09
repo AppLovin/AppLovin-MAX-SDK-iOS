b33a14afed1e518380c780f11fbc9e9551034490
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2022-05-09 10:07:53 -0700

    OguryPresage/2.6.1.1

--- OguryPresage/OguryPresageAdapter/ALOguryPresageMediationAdapter.m
+++ OguryPresage/OguryPresageAdapter/ALOguryPresageMediationAdapter.m
@@ -11,7 +11,7 @@
 #import <OguryAds/OguryAds.h>
 #import <OguryChoiceManager/OguryChoiceManager.h>
 
-#define ADAPTER_VERSION @"2.6.1.0"
+#define ADAPTER_VERSION @"2.6.1.1"
 
 @interface ALOguryPresageMediationAdapterInterstitialDelegate : NSObject<OguryInterstitialAdDelegate>
 @property (nonatomic,   weak) ALOguryPresageMediationAdapter *parentAdapter;
@@ -166,7 +166,7 @@ static MAAdapterInitializationStatus ALOguryPresageInitializationStatus = NSInte
     else
     {
         [self log: @"Interstitial ad not ready"];
-        [delegate didFailToDisplayInterstitialAdWithError: MAAdapterError.adNotReady];
+        [delegate didFailToDisplayInterstitialAdWithError: [MAAdapterError errorWithCode: -4205 errorString: @"Ad Display Failed"]];
     }
 }
 
@@ -222,7 +222,7 @@ static MAAdapterInitializationStatus ALOguryPresageInitializationStatus = NSInte
     else
     {
         [self log: @"Rewarded ad not ready"];
-        [delegate didFailToDisplayRewardedAdWithError: MAAdapterError.adNotReady];
+        [delegate didFailToDisplayRewardedAdWithError: [MAAdapterError errorWithCode: -4205 errorString: @"Ad Display Failed"]];
     }
 }
 
@@ -388,14 +388,15 @@ static MAAdapterInitializationStatus ALOguryPresageInitializationStatus = NSInte
 
 - (void)didFailOguryInterstitialAdWithError:(OguryError *)error forAd:(OguryInterstitialAd *)interstitial
 {
-    MAAdapterError *maxError = [ALOguryPresageMediationAdapter toMaxError: error];
     if ( [self.parentAdapter isShowing] )
     {
+        MAAdapterError *maxError = [MAAdapterError errorWithCode: -4205 errorString: @"Ad Display Failed" thirdPartySdkErrorCode: error.code thirdPartySdkErrorMessage: error.localizedDescription];
         [self.parentAdapter log: @"Interstitial (%@) failed to show with error: %@", self.adUnitIdentifier, maxError];
         [self.delegate didFailToDisplayInterstitialAdWithError: maxError];
     }
     else
     {
+        MAAdapterError *maxError = [ALOguryPresageMediationAdapter toMaxError: error];
         [self.parentAdapter log: @"Interstitial (%@) failed to load with error: %@", self.adUnitIdentifier, maxError];
         [self.delegate didFailToLoadInterstitialAdWithError: maxError];
     }
@@ -466,14 +467,15 @@ static MAAdapterInitializationStatus ALOguryPresageInitializationStatus = NSInte
 
 - (void)didFailOguryOptinVideoAdWithError:(OguryError *)error forAd:(OguryOptinVideoAd *)optinVideo
 {
-    MAAdapterError *maxError = [ALOguryPresageMediationAdapter toMaxError: error];
     if ( [self.parentAdapter isShowing] )
     {
+        MAAdapterError *maxError = [MAAdapterError errorWithCode: -4205 errorString: @"Ad Display Failed" thirdPartySdkErrorCode: error.code thirdPartySdkErrorMessage: error.localizedDescription];
         [self.parentAdapter log: @"Rewarded ad (%@) failed to show with error: %@", self.adUnitIdentifier, maxError];
-        [self.delegate didFailToDisplayRewardedAdWithError: maxError];
+        [self.delegate didFailToDisplayRewardedAdWithError: [MAAdapterError errorWithCode: -4205 errorString: @"Ad Display Failed" thirdPartySdkErrorCode: error.code thirdPartySdkErrorMessage: error.localizedDescription]];
     }
     else
     {
+        MAAdapterError *maxError = [ALOguryPresageMediationAdapter toMaxError: error];
         [self.parentAdapter log: @"Rewarded ad (%@) failed to load with error: %@", self.adUnitIdentifier, maxError];
         [self.delegate didFailToLoadRewardedAdWithError: maxError];
     }
