e24961e047d72fe70fb073da3719c275981edffb
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2022-06-30 04:06:05 -0700

    IronSource/7.2.3.0.0

--- IronSource/IronSourceAdapter/ALIronSourceMediationAdapter.m
+++ IronSource/IronSourceAdapter/ALIronSourceMediationAdapter.m
@@ -8,7 +8,7 @@
 #import "ALIronSourceMediationAdapter.h"
 #import <IronSource/IronSource.h>
 
-#define ADAPTER_VERSION @"7.2.2.1.0"
+#define ADAPTER_VERSION @"7.2.3.0.0"
 
 @interface ALIronSourceMediationAdapterRouter : ALMediationAdapterRouter<ISDemandOnlyInterstitialDelegate, ISDemandOnlyRewardedVideoDelegate, ISLogDelegate>
 @property (nonatomic, assign, getter=hasGrantedReward) BOOL grantedReward;
