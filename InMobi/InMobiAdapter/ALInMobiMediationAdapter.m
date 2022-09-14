1981b47c2b4e1c9b6686a2a3dc183eeabae30248
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2022-09-14 10:43:16 -0700

    InMobi/10.1.0.0

--- InMobi/InMobiAdapter/ALInMobiMediationAdapter.m
+++ InMobi/InMobiAdapter/ALInMobiMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALInMobiMediationAdapter.h"
 #import <InMobiSDK/InMobiSDK.h>
 
-#define ADAPTER_VERSION @"10.0.8.2"
+#define ADAPTER_VERSION @"10.1.0.0"
 
 /**
  * Dedicated delegate object for InMobi AdView ads.
