c7ca31147440980b18e17f4b2716423c518b470c
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2022-10-11 10:21:33 -0700

    Smaato/21.7.8.0

--- Smaato/CHANGELOG.md
+++ Smaato/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 21.7.8.0
+* Certified with Smaato SDK 21.7.8.
+
 ## 21.7.7.0
 * Certified with Smaato SDK 21.7.7.
 
--- Smaato/SmaatoAdapter/ALSmaatoMediationAdapter.m
+++ Smaato/SmaatoAdapter/ALSmaatoMediationAdapter.m
@@ -14,7 +14,7 @@
 #import <SmaatoSDKNative/SmaatoSDKNative.h>
 #import <SmaatoSDKInAppBidding/SmaatoSDKInAppBidding.h>
 
-#define ADAPTER_VERSION @"21.7.7.0"
+#define ADAPTER_VERSION @"21.7.8.0"
 
 /**
  * Router for interstitial/rewarded ad events.
