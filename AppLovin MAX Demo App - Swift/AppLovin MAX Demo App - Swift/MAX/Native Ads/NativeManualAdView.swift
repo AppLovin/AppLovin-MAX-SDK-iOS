a41764c98d96df934988c32b35de8f2416096e07
AviNLeung <123420316+AviNLeung@users.noreply.github.com>
2023-02-02 16:54:58 -0800

    Merge branch 'feat/add_star_rating' into ref/objective_c_star_rating_works

---
+++ AppLovin MAX Demo App - Swift/AppLovin MAX Demo App - Swift/MAX/Native Ads/NativeManualAdView.swift
@@ -0,0 +1,15 @@
+//
+//  NativeManualAdView.swift
+//  AppLovin MAX Demo App - Swift
+//
+//  Created by Avi Leung on 2/2/23.
+//  Copyright © 2023 AppLovin. All rights reserved.
+//
+
+import Foundation
+import AppLovinSDK
+class NativeManualAdView: MANativeAdView
+{
+
+    @IBOutlet var starRatingContentViewHeightConstraint: NSLayoutConstraint!
+}
