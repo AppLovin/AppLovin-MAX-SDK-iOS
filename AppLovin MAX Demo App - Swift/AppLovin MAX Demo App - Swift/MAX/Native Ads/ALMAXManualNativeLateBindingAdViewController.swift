641852995fe0a131f4e71f9438eb8bd1111e0eb4
AviNLeung <123420316+AviNLeung@users.noreply.github.com>
2023-02-06 13:20:58 -0800

    Update ALMAXManualNativeLateBindingAdViewController.swift

--- AppLovin MAX Demo App - Swift/AppLovin MAX Demo App - Swift/MAX/Native Ads/ALMAXManualNativeLateBindingAdViewController.swift
+++ AppLovin MAX Demo App - Swift/AppLovin MAX Demo App - Swift/MAX/Native Ads/ALMAXManualNativeLateBindingAdViewController.swift
@@ -63,7 +63,7 @@ class ALMAXManualNativeLateBindingAdViewController: ALBaseAdViewController
             builder.optionsContentViewTag = 1005
             builder.mediaContentViewTag = 1006
             builder.callToActionButtonTag = 1007
-            builder.starRatingContentViewTag =  1008
+            builder.starRatingContentViewTag = 1008
         })
         nativeAdView.bindViews(with: adViewBinder)
         
