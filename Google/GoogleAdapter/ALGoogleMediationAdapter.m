9184b40511593283306222cba59502986413adc4
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2023-04-14 21:59:09 -0700

    Google/10.3.0.2

--- Google/GoogleAdapter/ALGoogleMediationAdapter.m
+++ Google/GoogleAdapter/ALGoogleMediationAdapter.m
@@ -16,7 +16,7 @@
 #import "ALGoogleNativeAdViewDelegate.h"
 #import "ALGoogleNativeAdDelegate.h"
 
-#define ADAPTER_VERSION @"10.3.0.1"
+#define ADAPTER_VERSION @"10.3.0.2"
 
 @interface ALGoogleMediationAdapter ()
 
--- Google/GoogleAdapter/ALGoogleNativeAd.m
+++ Google/GoogleAdapter/ALGoogleNativeAd.m
@@ -8,6 +8,13 @@
 
 #import "ALGoogleNativeAd.h"
 
+#define TITLE_LABEL_TAG          1
+#define MEDIA_VIEW_CONTAINER_TAG 2
+#define ICON_VIEW_TAG            3
+#define BODY_VIEW_TAG            4
+#define CALL_TO_ACTION_VIEW_TAG  5
+#define ADVERTISER_VIEW_TAG      8
+
 @interface ALGoogleMediationAdapter ()
 @property (nonatomic, strong) GADNativeAd *nativeAd;
 @property (nonatomic, strong) GADNativeAdView *nativeAdView;
@@ -35,16 +42,47 @@
 
 - (void)prepareViewForInteraction:(MANativeAdView *)maxNativeAdView
 {
+    NSMutableArray *clickableViews = [NSMutableArray array];
+    if ( [self.title al_isValidString] && maxNativeAdView.titleLabel )
+    {
+        [clickableViews addObject: maxNativeAdView.titleLabel];
+    }
+    if ( [self.advertiser al_isValidString] && maxNativeAdView.advertiserLabel )
+    {
+        [clickableViews addObject: maxNativeAdView.advertiserLabel];
+    }
+    if ( [self.body al_isValidString] && maxNativeAdView.bodyLabel )
+    {
+        [clickableViews addObject: maxNativeAdView.bodyLabel];
+    }
+    if ( [self.callToAction al_isValidString] && maxNativeAdView.callToActionButton )
+    {
+        [clickableViews addObject: maxNativeAdView.callToActionButton];
+    }
+    if ( self.icon && maxNativeAdView.iconImageView )
+    {
+        [clickableViews addObject: maxNativeAdView.iconImageView];
+    }
+    if ( self.mediaView && maxNativeAdView.mediaContentView )
+    {
+        [clickableViews addObject: maxNativeAdView.mediaContentView];
+    }
+    
+    [self prepareForInteractionClickableViews: clickableViews withContainer: maxNativeAdView];
+}
+
+- (BOOL)prepareForInteractionClickableViews:(NSArray<UIView *> *)clickableViews withContainer:(UIView *)container
+{
     GADNativeAd *nativeAd = self.parentAdapter.nativeAd;
     if ( !nativeAd )
     {
         [self.parentAdapter e: @"Failed to register native ad views: native ad is nil."];
-        return;
+        return NO;
     }
     
     // Check if the publisher included Google's `GADNativeAdView`. If we can use an integrated view, Google
     // won't need to overlay the view on top of the pub view, causing unrelated buttons to be unclickable
-    GADNativeAdView *gadNativeAdView = [maxNativeAdView viewWithTag: self.gadNativeAdViewTag];
+    GADNativeAdView *gadNativeAdView = [container viewWithTag: self.gadNativeAdViewTag];
     if ( ![gadNativeAdView isKindOfClass: [GADNativeAdView class]] )
     {
         gadNativeAdView = [[GADNativeAdView alloc] init];
@@ -53,38 +91,75 @@
         self.parentAdapter.nativeAdView = gadNativeAdView;
         
         // NOTE: iOS needs order to be maxNativeAdView -> gadNativeAdView in order for assets to be sized correctly
-        [maxNativeAdView addSubview: gadNativeAdView];
+        [container addSubview: gadNativeAdView];
         
         // Pin view in order to make it clickable - this makes views not registered with the native ad view unclickable
         [gadNativeAdView al_pinToSuperview];
     }
     
-    gadNativeAdView.iconView = maxNativeAdView.iconImageView;
-    gadNativeAdView.headlineView = maxNativeAdView.titleLabel;
-    gadNativeAdView.bodyView = maxNativeAdView.bodyLabel;
-    gadNativeAdView.callToActionView = maxNativeAdView.callToActionButton;
-    gadNativeAdView.callToActionView.userInteractionEnabled = NO;
-    
-    if ( [self.mediaView isKindOfClass: [GADMediaView class]] )
+    // Native integrations
+    if ( [container isKindOfClass: [MANativeAdView class]] )
     {
-        gadNativeAdView.mediaView = (GADMediaView *) self.mediaView;
+        MANativeAdView *maxNativeAdView = (MANativeAdView *) container;
+        gadNativeAdView.headlineView = maxNativeAdView.titleLabel;
+        gadNativeAdView.advertiserView = maxNativeAdView.advertiserLabel;
+        gadNativeAdView.bodyView = maxNativeAdView.bodyLabel;
+        gadNativeAdView.iconView = maxNativeAdView.iconImageView;
+        gadNativeAdView.callToActionView = maxNativeAdView.callToActionButton;
+        gadNativeAdView.callToActionView.userInteractionEnabled = NO;
+        
+        if ( [self.mediaView isKindOfClass: [GADMediaView class]] )
+        {
+            gadNativeAdView.mediaView = (GADMediaView *) self.mediaView;
+        }
+        else if ( [self.mediaView isKindOfClass: [UIImageView class]] )
+        {
+            gadNativeAdView.imageView = self.mediaView;
+        }
     }
-    else if ( [self.mediaView isKindOfClass: [UIImageView class]] )
+    // Plugins
+    else
     {
-        gadNativeAdView.imageView = self.mediaView;
+        for ( UIView *clickableView in clickableViews )
+        {
+            if ( clickableView.tag == TITLE_LABEL_TAG )
+            {
+                gadNativeAdView.headlineView = clickableView;
+            }
+            else if ( clickableView.tag == ICON_VIEW_TAG )
+            {
+                gadNativeAdView.iconView = clickableView;
+            }
+            else if ( clickableView.tag == MEDIA_VIEW_CONTAINER_TAG )
+            {
+                // `self.mediaView` is created when ad is loaded
+                if ( [self.mediaView isKindOfClass: [GADMediaView class]] )
+                {
+                    gadNativeAdView.mediaView = (GADMediaView *) self.mediaView;
+                }
+                else if ( [self.mediaView isKindOfClass: [UIImageView class]] )
+                {
+                    gadNativeAdView.imageView = self.mediaView;
+                }
+            }
+            else if ( clickableView.tag == BODY_VIEW_TAG )
+            {
+                gadNativeAdView.bodyView = clickableView;
+            }
+            else if ( clickableView.tag == CALL_TO_ACTION_VIEW_TAG )
+            {
+                gadNativeAdView.callToActionView = clickableView;
+            }
+            else if ( clickableView.tag == ADVERTISER_VIEW_TAG )
+            {
+                gadNativeAdView.advertiserView = clickableView;
+            }
+        }
     }
-    
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wundeclared-selector"
-    // Introduced in 10.4.0
-    if ( [maxNativeAdView respondsToSelector: @selector(advertiserLabel)] )
-    {
-        id advertiserLabel = [maxNativeAdView performSelector: @selector(advertiserLabel)];
-        gadNativeAdView.advertiserView = advertiserLabel;
-    }
-#pragma clang diagnostic pop
     
     gadNativeAdView.nativeAd = self.parentAdapter.nativeAd;
+    
+    return YES;
 }
 
 @end
