41bb70c1fa8360d07078beb73d0f07b09b035833
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2023-04-18 05:09:50 -0700

    Tapjoy/13.0.1.0

--- Tapjoy/TapjoyAdapter/ALTapjoyMediationAdapter.m
+++ Tapjoy/TapjoyAdapter/ALTapjoyMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALTapjoyMediationAdapter.h"
 #import <Tapjoy/Tapjoy.h>
 
-#define ADAPTER_VERSION @"13.0.0.0"
+#define ADAPTER_VERSION @"13.0.1.0"
 
 @interface ALTapjoyMediationAdapterInterstitialDelegate : NSObject <TJPlacementDelegate, TJPlacementVideoDelegate>
 @property (nonatomic,   weak) ALTapjoyMediationAdapter *parentAdapter;
