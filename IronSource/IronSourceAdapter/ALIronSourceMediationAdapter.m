6a29c15cc80d7ac82a26fc4c9d59ad9f4441092d
alvarshahanji <48224559+alvarshahanji@users.noreply.github.com>
2023-05-17 20:31:01 -0700

    IronSource/7.3.0.0.0 (#227)
    https://applovinteam.slack.com/archives/C02SARXQ9R9/p1684376523564989
    
    completing iOS push

--- IronSource/IronSourceAdapter/ALIronSourceMediationAdapter.m
+++ IronSource/IronSourceAdapter/ALIronSourceMediationAdapter.m
@@ -8,7 +8,7 @@
 #import "ALIronSourceMediationAdapter.h"
 #import <IronSource/IronSource.h>
 
-#define ADAPTER_VERSION @"7.2.7.0.1"
+#define ADAPTER_VERSION @"7.3.0.0.0"
 
 @interface ALIronSourceMediationAdapterRouter : ALMediationAdapterRouter <ISDemandOnlyInterstitialDelegate, ISDemandOnlyRewardedVideoDelegate, ISLogDelegate>
 @property (nonatomic, assign, getter=hasGrantedReward) BOOL grantedReward;
