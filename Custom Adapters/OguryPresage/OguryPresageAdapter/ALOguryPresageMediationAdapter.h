f092e50661341602e6f13d4d25bba826ca034fcb
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2023-05-23 12:09:19 -0700

    OguryPresage/4.1.2.1

---
+++ Custom Adapters/OguryPresage/OguryPresageAdapter/ALOguryPresageMediationAdapter.h
@@ -0,0 +1,13 @@
+//
+//  ALOguryPresageMediationAdapter.h
+//  AppLovinSDK
+//
+//  Created by Ritam Sarmah on 1/14/21.
+//  Copyright © 2022 AppLovin Corporation. All rights reserved.
+//
+
+#import <AppLovinSDK/AppLovinSDK.h>
+
+@interface ALOguryPresageMediationAdapter : ALMediationAdapter <MASignalProvider, MAInterstitialAdapter, MARewardedAdapter, MAAdViewAdapter>
+
+@end
