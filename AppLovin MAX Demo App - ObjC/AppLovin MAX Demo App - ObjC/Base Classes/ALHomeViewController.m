72cb7e1e9cba7da26a150d1a4f86d13eb920739f
andy0liang <andy.liang@applovin.com>
2023-07-05 15:49:50 -0700

    Update ALHomeViewController.m

--- AppLovin MAX Demo App - ObjC/AppLovin MAX Demo App - ObjC/Base Classes/ALHomeViewController.m
+++ AppLovin MAX Demo App - ObjC/AppLovin MAX Demo App - ObjC/Base Classes/ALHomeViewController.m
@@ -19,6 +19,11 @@
 static NSString *const kSupportLink = @"https://support.applovin.com/support/home";
 static const NSInteger kRowIndexToHideForPhone = 3;
 
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleDefault;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -93,10 +98,6 @@ static const NSInteger kRowIndexToHideForPhone = 3;
     {
         [[UIApplication sharedApplication] openURL: [NSURL URLWithString: kSupportLink]];
     }
-}
-
-- (UIStatusBarStyle) preferredStatusBarStyle {
-    return UIStatusBarStyleDefault;
 }
 
 - (void)addFooterLabel
