1793bf39cc1554171ab13cf0578badbaa7671a7e
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2023-08-16 11:13:30 -0700

    Smaato/22.3.0.0

--- Smaato/SmaatoAdapter/ALSmaatoMediationAdapter.m
+++ Smaato/SmaatoAdapter/ALSmaatoMediationAdapter.m
@@ -14,7 +14,7 @@
 #import <SmaatoSDKNative/SmaatoSDKNative.h>
 #import <SmaatoSDKInAppBidding/SmaatoSDKInAppBidding.h>
 
-#define ADAPTER_VERSION @"22.2.1.0"
+#define ADAPTER_VERSION @"22.3.0.0"
 
 /**
  * Router for interstitial/rewarded ad events.
