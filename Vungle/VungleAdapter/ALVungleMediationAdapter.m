4f67ed2253f4b5a3256117133ab1ab8af8e8292c
Manoj Budumuru <manoj.budumuru@vungle.com>
2023-09-08 10:21:16 -0700

    AdapterVersion update.

--- Vungle/VungleAdapter/ALVungleMediationAdapter.m
+++ Vungle/VungleAdapter/ALVungleMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALVungleMediationAdapter.h"
 #import <VungleAdsSDK/VungleAdsSDK.h>
 
-#define ADAPTER_VERSION @"7.0.1.0"
+#define ADAPTER_VERSION @"7.1.0.0"
 
 @interface ALVungleMediationAdapterInterstitialAdDelegate : NSObject <VungleInterstitialDelegate>
 @property (nonatomic,   weak) ALVungleMediationAdapter *parentAdapter;
