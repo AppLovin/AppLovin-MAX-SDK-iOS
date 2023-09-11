75dfea1afad9aceac596a3dd97e301bf0e280084
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2023-09-11 03:36:05 -0700

    Mintegral/7.4.4.0.0

--- Mintegral/MintegralAdapter/ALMintegralMediationAdapter.m
+++ Mintegral/MintegralAdapter/ALMintegralMediationAdapter.m
@@ -15,7 +15,7 @@
 #import <MTGSDKBanner/MTGBannerAdViewDelegate.h>
 #import <MTGSDKSplash/MTGSplashAD.h>
 
-#define ADAPTER_VERSION @"7.4.3.0.0"
+#define ADAPTER_VERSION @"7.4.4.0.0"
 
 // List of Mintegral error codes not defined in API, but in their docs
 //
