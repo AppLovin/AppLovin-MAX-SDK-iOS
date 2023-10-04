1822d93109b955659eff68f61c1b616796bc1277
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2023-10-04 15:36:12 -0700

    Google/10.12.0.0

--- Google/GoogleAdapter/ALGoogleMediationAdapter.m
+++ Google/GoogleAdapter/ALGoogleMediationAdapter.m
@@ -16,7 +16,7 @@
 #import "ALGoogleNativeAdViewDelegate.h"
 #import "ALGoogleNativeAdDelegate.h"
 
-#define ADAPTER_VERSION @"10.11.0.0"
+#define ADAPTER_VERSION @"10.12.0.0"
 
 @interface ALGoogleMediationAdapter ()
 
