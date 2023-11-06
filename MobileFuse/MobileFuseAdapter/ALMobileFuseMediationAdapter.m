109841a7470e295d9e2503b6cde5c479b4d59587
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2023-11-06 10:31:01 -0800

    MobileFuse/1.6.5.0

--- MobileFuse/MobileFuseAdapter/ALMobileFuseMediationAdapter.m
+++ MobileFuse/MobileFuseAdapter/ALMobileFuseMediationAdapter.m
@@ -16,7 +16,7 @@
 #import <MobileFuseSDK/MFRewardedAd.h>
 #import <MobileFuseSDK/MFNativeAd.h>
 
-#define ADAPTER_VERSION @"1.6.4.0"
+#define ADAPTER_VERSION @"1.6.5.0"
 
 /**
  * Enum representing the list of MobileFuse SDK error codes in https://docs.mobilefuse.com/docs/error-codes.
