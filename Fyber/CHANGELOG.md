8f14473b147705a14dd2086295015f9aaf623dbc
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-03-13 09:43:52 -0700

    Fyber/8.2.7.0

--- Fyber/CHANGELOG.md
+++ Fyber/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 8.2.7.0
+* Certified with Fyber SDK 8.2.7.
+
 ## 8.2.6.0
 * Certified with Fyber SDK 8.2.6.
 
--- Fyber/Fyber/ALInneractiveMediationAdapter.m
+++ Fyber/Fyber/ALInneractiveMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALInneractiveMediationAdapter.h"
 #import <IASDKCore/IASDKCore.h>
 
-#define ADAPTER_VERSION @"8.2.6.0"
+#define ADAPTER_VERSION @"8.2.7.0"
 
 @interface ALInneractiveMediationAdapterGlobalDelegate : NSObject <IAGlobalAdDelegate>
 @end
