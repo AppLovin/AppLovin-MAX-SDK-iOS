3f29b122e93f448ad684eaf86a02aa4cc71a7bc0
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-04-11 15:37:07 -0700

    AmazonAdMarketplace/4.9.2.0

--- AmazonAdMarketplace/AmazonAdMarketplaceAdapter/ALAmazonAdMarketplaceMediationAdapter.m
+++ AmazonAdMarketplace/AmazonAdMarketplaceAdapter/ALAmazonAdMarketplaceMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALAmazonAdMarketplaceMediationAdapter.h"
 #import <DTBiOSSDK/DTBiOSSDK.h>
 
-#define ADAPTER_VERSION @"4.9.1.0"
+#define ADAPTER_VERSION @"4.9.2.0"
 
 /**
  * Container object for holding mediation hints dict generated from Amazon's SDK and the timestamp it was geenrated at.
--- AmazonAdMarketplace/CHANGELOG.md
+++ AmazonAdMarketplace/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 4.9.2.0
+* Certified with AmazonAdMarketplace SDK 4.9.2.
+
 ## 4.9.1.0
 * Certified with AmazonAdMarketplace SDK 4.9.1.
 * Remove deprecated callbacks `didStartRewardedAdVideo` and `didCompleteRewardedAdVideo`.
