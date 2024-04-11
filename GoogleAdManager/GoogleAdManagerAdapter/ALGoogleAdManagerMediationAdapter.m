8b88106251eb88cdfdd2dcb8bb4b5db2f4e501c4
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-04-11 16:01:18 -0700

    GoogleAdManager/11.3.0.0

--- GoogleAdManager/GoogleAdManagerAdapter/ALGoogleAdManagerMediationAdapter.m
+++ GoogleAdManager/GoogleAdManagerAdapter/ALGoogleAdManagerMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALGoogleAdManagerMediationAdapter.h"
 #import <GoogleMobileAds/GoogleMobileAds.h>
 
-#define ADAPTER_VERSION @"11.2.0.1"
+#define ADAPTER_VERSION @"11.3.0.0"
 
 #define TITLE_LABEL_TAG          1
 #define MEDIA_VIEW_CONTAINER_TAG 2
