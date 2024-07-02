5be5e9393fa3cafe05be41104aec89460ab3c151
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-07-02 07:32:37 -0700

    Fyber/8.3.1.0

--- Fyber/Fyber/ALInneractiveMediationAdapter.m
+++ Fyber/Fyber/ALInneractiveMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALInneractiveMediationAdapter.h"
 #import <IASDKCore/IASDKCore.h>
 
-#define ADAPTER_VERSION @"8.3.0.0"
+#define ADAPTER_VERSION @"8.3.1.0"
 
 @interface ALInneractiveMediationAdapterGlobalDelegate : NSObject <IAGlobalAdDelegate>
 @end
