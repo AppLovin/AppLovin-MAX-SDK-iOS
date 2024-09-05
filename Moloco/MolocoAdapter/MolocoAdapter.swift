cdf34f0fa076e72cb144a2ab95ee31cbdb2b19dd
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-09-05 08:43:40 -0700

    Moloco/3.1.3.0

--- Moloco/MolocoAdapter/MolocoAdapter.swift
+++ Moloco/MolocoAdapter/MolocoAdapter.swift
@@ -36,7 +36,7 @@ final class MolocoAdapter: ALMediationAdapter
     
     override var thirdPartySdkName: String { "Moloco" }
     
-    override var adapterVersion: String { "3.1.2.0" }
+    override var adapterVersion: String { "3.1.3.0" }
     
     override var sdkVersion: String { Moloco.shared.sdkVersion }
     
