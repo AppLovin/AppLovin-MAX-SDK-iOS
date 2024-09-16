9bc1fba0f0a2f8854052c338f1789169f42c3e87
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-09-15 17:24:29 -0700

    MobileFuse/1.7.6.1

--- MobileFuse/AppLovinMediationMobileFuseAdapter.podspec
+++ MobileFuse/AppLovinMediationMobileFuseAdapter.podspec
@@ -2,7 +2,7 @@ Pod::Spec.new do |s|
 
 s.authors = 'AppLovin Corporation'
 s.name = 'AppLovinMediationMobileFuseAdapter'
-s.version = '1.7.6.0'
+s.version = '1.7.6.1'
 s.platform = :ios, '11.0'
 s.summary = 'MobileFuse adapter used for mediation with the AppLovin MAX SDK'
 s.homepage = "https://github.com/CocoaPods/Specs/search?o=desc&q=#{s.name}&s=indexed"
@@ -27,7 +27,7 @@ s.source =
 s.vendored_frameworks = "#{s.name}-#{s.version}/#{s.name}.xcframework"
 
 s.dependency 'MobileFuseSDK', '= 1.7.6'
-s.dependency 'AppLovinSDK'
+s.dependency 'AppLovinSDK', '>= 13.0.0'
 s.swift_version = '5.0'
 
 s.pod_target_xcconfig =
--- MobileFuse/MobileFuseAdapter/ALMobileFuseMediationAdapter.m
+++ MobileFuse/MobileFuseAdapter/ALMobileFuseMediationAdapter.m
@@ -17,7 +17,7 @@
 #import <MobileFuseSDK/MFNativeAd.h>
 #import <MobileFuseSDK/MobileFusePrivacyPreferences.h>
 
-#define ADAPTER_VERSION @"1.7.6.0"
+#define ADAPTER_VERSION @"1.7.6.1"
 
 /**
  * Enum representing the list of MobileFuse SDK error codes in https://docs.mobilefuse.com/docs/error-codes.
@@ -391,12 +391,6 @@ static NSString *ALMobileFuseSDKVersion;
     else
     {
         [privacyPreferences setUsPrivacyConsentString: @"1---"];
-    }
-    
-    NSNumber *isAgeRestrictedUser = [parameters isAgeRestrictedUser];
-    if ( isAgeRestrictedUser != nil )
-    {
-        [privacyPreferences setSubjectToCoppa: isAgeRestrictedUser.boolValue];
     }
     
     if ( parameters.consentString )
@@ -953,11 +947,6 @@ static NSString *ALMobileFuseSDKVersion;
         self.parentAdapter = parentAdapter;
     }
     return self;
-}
-
-- (void)prepareViewForInteraction:(MANativeAdView *)maxNativeAdView
-{
-    [self prepareForInteractionClickableViews: [self.parentAdapter clickableViewsForNativeAdView: maxNativeAdView] withContainer: maxNativeAdView];
 }
 
 - (BOOL)prepareForInteractionClickableViews:(NSArray<UIView *> *)clickableViews withContainer:(UIView *)container
