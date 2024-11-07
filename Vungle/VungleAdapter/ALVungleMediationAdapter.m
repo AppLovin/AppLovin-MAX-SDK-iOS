ecb0619497f9abe52fa179b37f133d5a63db2b6c
ManojBudumuru-vungle <63756359+ManojBudumuru-vungle@users.noreply.github.com>
2024-11-07 12:53:56 -0800

    Max error code mapping update (#21)
    This commit will update the way errors are mapped in mediation
    
    IOS-7170

--- Vungle/VungleAdapter/ALVungleMediationAdapter.m
+++ Vungle/VungleAdapter/ALVungleMediationAdapter.m
@@ -566,8 +566,19 @@ static MAAdapterInitializationStatus ALVungleIntializationStatus = NSIntegerMin;
         case VungleErrorAdPublisherMismatch:
             adapterError = MAAdapterError.invalidConfiguration;
             break;
+        case VungleErrorInvalidPlayParameter:
+            adapterError = MAAdapterError.missingViewController;
+            break;
         case VungleErrorJsonEncodeError:
         case VungleErrorAdInternalIntegrationError:
+        case VungleErrorConfigNotFoundError:
+        case VungleErrorInvalidRequestBuilderError:
+        case VungleErrorMraidJsWriteFailed:
+        case VungleErrorMraidDownloadJsError:
+        case VungleErrorMraidJsDoesNotExist:
+        case VungleErrorMraidJsCopyFailed:
+        case VungleErrorTemplateUnzipError:
+        case VungleErrorAssetWriteError:
             adapterError = MAAdapterError.internalError;
             break;
         case VungleErrorAdConsumed:
@@ -575,6 +586,21 @@ static MAAdapterInitializationStatus ALVungleIntializationStatus = NSIntegerMin;
         case VungleErrorAdAlreadyLoaded:
         case VungleErrorAdIsPlaying:
         case VungleErrorAdAlreadyFailed:
+        case VungleErrorInvalidGzipBidPayload:
+        case VungleErrorInvalidBidPayload:
+        case VungleErrorInvalidJsonBidPayload:
+        case VungleErrorInvalidAdunitBidPayload:
+        case VungleErrorAdResponseEmpty:
+        case VungleErrorInvalidEventIDError:
+        case VungleErrorApiRequestError:
+        case VungleErrorApiResponseDataError:
+        case VungleErrorApiResponseDecodeError:
+        case VungleErrorApiFailedStatusCode:
+        case VungleErrorInvalidTemplateURL:
+        case VungleErrorInvalidAssetURL:
+        case VungleErrorAssetRequestError:
+        case VungleErrorAssetResponseDataError:
+        case VungleErrorAssetFailedStatusCode:
             adapterError = MAAdapterError.invalidLoadState;
             break;
         case VungleErrorAdNotLoaded:
@@ -584,6 +610,8 @@ static MAAdapterInitializationStatus ALVungleIntializationStatus = NSIntegerMin;
         case VungleErrorInvalidIfaStatus:
         case VungleErrorMraidBridgeError:
         case VungleErrorConcurrentPlaybackUnsupported:
+        case VungleErrorAdClosedTemplateError:
+        case VungleErrorAdClosedMissingHeartbeat:
             adapterError = MAAdapterError.adDisplayFailedError;
             break;
         case VungleErrorPlacementSleep:
@@ -608,6 +636,7 @@ static MAAdapterInitializationStatus ALVungleIntializationStatus = NSIntegerMin;
             break;
         case VungleErrorWebViewWebContentProcessDidTerminate:
         case VungleErrorWebViewFailedNavigation:
+        case VungleErrorWebviewError:
             adapterError = MAAdapterError.webViewError;
             break;
     }
