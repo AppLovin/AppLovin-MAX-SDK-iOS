6531a750ec2388497ef824b753cb7c949da728e8
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-01-03 01:33:52 -0800

    ByteDance/6.4.1.0.0

--- ByteDance/ByteDanceAdapter/ALByteDanceMediationAdapter.m
+++ ByteDance/ByteDanceAdapter/ALByteDanceMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALByteDanceMediationAdapter.h"
 #import <PAGAdSDK/PAGAdSDK.h>
 
-#define ADAPTER_VERSION @"6.4.0.9.0"
+#define ADAPTER_VERSION @"6.4.1.0.0"
 
 @interface ALByteDanceInterstitialAdDelegate : NSObject <PAGLInterstitialAdDelegate>
 @property (nonatomic,   weak) ALByteDanceMediationAdapter *parentAdapter;
--- ByteDance/CHANGELOG.md
+++ ByteDance/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 6.4.1.0.0
+* Certified with ByteDance SDK 6.4.1.0.
+
 ## 6.4.0.9.0
 * Certified with ByteDance SDK 6.4.0.9.
 
