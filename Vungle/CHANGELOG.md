e2c23026cf095728f954f9ab32d2d92c3346021f
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-08-06 20:36:12 -0700

    Vungle/7.5.3.0

--- Vungle/CHANGELOG.md
+++ Vungle/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 7.5.3.0
+* Certified with Vungle SDK 7.5.3.
+
 ## 7.5.2.0
 * Certified with Vungle SDK 7.5.2.
 
--- Vungle/VungleAdapter/ALVungleMediationAdapter.m
+++ Vungle/VungleAdapter/ALVungleMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALVungleMediationAdapter.h"
 #import <VungleAdsSDK/VungleAdsSDK.h>
 
-#define ADAPTER_VERSION @"7.5.2.0"
+#define ADAPTER_VERSION @"7.5.3.0"
 
 @interface ALVungleMediationAdapterInterstitialAdDelegate : NSObject <VungleInterstitialDelegate>
 @property (nonatomic,   weak) ALVungleMediationAdapter *parentAdapter;
