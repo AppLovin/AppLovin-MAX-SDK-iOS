746a968a117aab79a2dd1879e6543af30f6be5fd
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2026-01-22 19:36:03 -0800

    UnityAds/4.16.6.0

--- UnityAds/UnityAdsAdapter/ALUnityAdsMediationAdapter.m
+++ UnityAds/UnityAdsAdapter/ALUnityAdsMediationAdapter.m
@@ -9,7 +9,7 @@
 #import "ALUnityAdsMediationAdapter.h"
 #import <UnityAds/UnityAds.h>
 
-#define ADAPTER_VERSION @"4.16.5.0"
+#define ADAPTER_VERSION @"4.16.6.0"
 
 @interface ALUnityAdsInitializationDelegate : NSObject <UnityAdsInitializationDelegate>
 @property (nonatomic, weak) ALUnityAdsMediationAdapter *parentAdapter;
